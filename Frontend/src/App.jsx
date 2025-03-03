27d6bd624be7dcbab8c37d3e8d1e32fe3e3285b1
KavishkaSasindu <sasindu0215@gmail.com>
2025-03-03 18:59:13 +0530

    initialize commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            <div className="text-blue-700">Library Management System</div>
+        </div>
+    )
+}
+export default App
